148b0bc87664cfade9248ba9d29b496ce210506a
jgaviola <jgaviola0527@gmail.com>
2016-11-17 14:34:57 -0500

    Add files via upload

---
+++ Thw.c
@@ -0,0 +1,23 @@
+//Joshua Gaviola
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(void) {
+
+//Random # Gen
+srand(getpid());
+
+//Variables
+int t, c, s, q, g, m, p, a;
+
+//Code????
+printf("Welcome to the Thanksgiving Shopping List Calculator.\n");
+printf("Enter 't' for Turkey\n");
+printf("Enter 'c' for Cranberry Sauce\n");
+printf("Enter 's' for Stuffing\n");
+printf("Enter 'q' for Squash\n");
+printf("Enter 'g' for Gravy\n");
+printf("Enter 'm' for Mashed Potatoes\n");
+printf("Enter 'p' for Sweet Potatoes\n");
+printf("Enter 'a' for Apple Pie\n");
+}
