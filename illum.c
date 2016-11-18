1bac967382f59144dc28bce061df258f43825649
jgaviola <jgaviola0527@gmail.com>
2016-11-18 14:42:23 -0500

    Add files via upload

---
+++ illum.c
@@ -0,0 +1,31 @@
+/*Joshua Gaviola*/
+#include <stdio.h>
+int main(void) {
+
+	int h, w, q, f, g;
+
+	printf("Please enter a number: ");
+	scanf("%d", &q);
+
+	while (q != 4) {
+		if (q == 1) {
+			printf("Width:");
+			scanf("%d", &w);
+			printf("Height:");
+			scanf("%d", &h);
+
+			for (f = 0; f <= w; f++) {
+				printf("*");
+				for (w = 
+	}
+
+}}
+//Triangle stuff dont touch
+/*	for (f = 0; f < h; f++) {
+		for(g = 0; g <= f; g++) {
+			printf("*");
+		}
+	printf("\n");
+	}*/
+return 0;
+}
